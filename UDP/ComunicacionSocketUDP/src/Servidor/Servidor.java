d394aa7ab201f74c1cbafc03845ccf3af0f1bd04
Fabio Andrés López Corredor <fa.lopez10@uniandes.edu.co>
2017-04-05 21:18:29 -0500

    Cambios hechos en el laboratorio el 4 de abril.
    Codigo de presentaciones y páginas consultadas

--- UDP/ComunicacionSocketUDP/src/Servidor/Servidor.java
+++ UDP/ComunicacionSocketUDP/src/Servidor/Servidor.java
@@ -7,6 +7,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -15,50 +16,23 @@ import java.net.SocketTimeoutException;
 
 public class Servidor 
 {
-	public static void main(String argv[]) throws Exception
-	{	
-
-		//Creamos el socket que escucha en el puerto 6789
-		ServerSocket welcomeSocket = new ServerSocket(6789);
-
-		//Establecemos el tamaño del buffer
-		welcomeSocket.setReceiveBufferSize(12);
-
-		//Archivo
-		File archivo;
-
-		//Establecemos el timeout del socket
-		welcomeSocket.setSoTimeout(30000);
-
-		
-
-		//Rutas de los archivos a descargar
-		while(true) 
-		{
-			try {
-
-				Socket conn = welcomeSocket.accept();
-
-				DataInputStream flujo = new DataInputStream(conn.getInputStream());
-
-				String r = flujo.readUTF();
-
-				System.out.println(r);
-				
-				
-
-
-				DataOutputStream salida = new DataOutputStream(conn.getOutputStream());
-				
-//				salida.writeUTF("Se envio el archivo...");
-				conn.close();
-
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				System.out.println("Socket timed out!");
-				break;
-			}		     
-		}
-
-	}
+	public static void main(String args[]) throws IOException {
+        ServerSocket ss;
+        System.out.print("Inicializando servidor... ");
+        try {
+            ss = new ServerSocket(9876);
+            System.out.println("\t[OK]");
+            int idSession = 0;
+            while (true) {
+                Socket socket;
+                System.out.println("No se muere antes del accept");
+                socket = ss.accept();
+                System.out.println("Nueva conexión entrante: "+socket);
+                ((ThreadServidor) new ThreadServidor(socket, idSession)).start();
+                idSession++;
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
